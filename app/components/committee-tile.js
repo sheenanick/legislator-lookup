c1c7e699c572d54dc8e309c17db2e94ea2776f6f
Kevin Johnston and Sheena Nickerson <kevin@>
2016-11-01 13:51:17 -0700

    add functionality to filter only house committees

--- app/components/committee-tile.js
+++ app/components/committee-tile.js
@@ -1,8 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  all: true,
+  house: false,
   actions: {
-    filter() {
+    filter(params) {
+      if(params === 'house'){
+        this.set('house', true);
+        this.set('all', false);
+      }
+      if(params === 'all'){
+        this.set('house', false);
+        this.set('all', true);
+      }
 
     }
   }
--- app/routes/committee.js
+++ app/routes/committee.js
@@ -2,11 +2,17 @@ import Ember from 'ember';
 import config from '../config/environment';
 
 export default Ember.Route.extend({
-  model: function() {
+  model() {
     var key = config.myApiKey;
     var url = 'http://congress.api.sunlightfoundation.com/committees?subcommittee=false&apikey=' + key;
-    return Ember.$.getJSON(url).then(function(responseJSON) {
-      return responseJSON.results;
+    var url2 = 'http://congress.api.sunlightfoundation.com/committees?chamber=house&subcommittee=false&apikey=' + key;
+    return Ember.RSVP.hash({
+      all: Ember.$.getJSON(url).then(function(responseJSON) {
+        return responseJSON.results;
+      }),
+      house: Ember.$.getJSON(url2).then(function(responseJSON) {
+        return responseJSON.results;
+      })
     });
   }
 });
